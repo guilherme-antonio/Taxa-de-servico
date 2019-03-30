420d6b1fb7e3cce9693f70d571ada9c838c1eac0
aluno <guioliveira99@gmail.com>
2019-03-29 21:50:48 -0300

    Creating tax and total values and calculations

--- app/src/main/java/com/example/taxadeservico/MainActivity.java
+++ app/src/main/java/com/example/taxadeservico/MainActivity.java
@@ -2,16 +2,24 @@ package com.example.taxadeservico;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText value;
     SeekBar seekBar;
     TextView textView;
+    TextView tax;
+    TextView total;
     NumberFormat formatacaoPercentual = NumberFormat.getPercentInstance();
+    NumberFormat formatacaoCurrency = NumberFormat.getCurrencyInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,26 @@ public class MainActivity extends AppCompatActivity {
 
         seekBar = (SeekBar) findViewById(R.id.seekbar);
         textView = (TextView) findViewById(R.id.text);
+        tax = (TextView) findViewById(R.id.tax);
+        total = (TextView) findViewById(R.id.total);
+        value = (EditText) findViewById(R.id.value);
+
+        value.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                calcularValores();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -44,5 +72,20 @@ public class MainActivity extends AppCompatActivity {
         int valor = seekBar.getProgress();
 
         this.textView.setText(formatacaoPercentual.format(Double.valueOf(valor)/100));
+
+        calcularValores();
+    }
+
+    public void calcularValores(){
+        float valor = Float.valueOf(value.getText().toString());
+
+        int porcentagem = seekBar.getProgress();
+
+        float taxa = valor * (Float.valueOf(porcentagem) / 100);
+
+        float totalValue = valor + taxa;
+
+        tax.setText(formatacaoCurrency.format(taxa));
+        total.setText(formatacaoCurrency.format(totalValue));
     }
 }
