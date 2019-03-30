76826b797cf3adea68b55a62e1d3f5303648a59a
aluno <guioliveira99@gmail.com>
2019-03-29 21:09:29 -0300

    Seekbar creation

--- app/src/main/java/com/example/taxadeservico/MainActivity.java
+++ app/src/main/java/com/example/taxadeservico/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.taxadeservico;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar seekBar;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        seekBar = (SeekBar) findViewById(R.id.seekbar);
+        textView = (TextView) findViewById(R.id.text);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                sincronizaTextView();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void sincronizaTextView(){
+        int valor = seekBar.getProgress();
+
+        this.textView.setText(String.format("%d%%", valor));
     }
 }
